a30ab97cd8b784998327ccd025bd506c6adc1401
btnguyenPersonal <btnguyenPersonal@github.com>
2022-06-24 23:15:49 -0400

    stuck at pathfinding, hard in C

--- pathfinding.c
+++ pathfinding.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define LENGTH 10
+
 void readFile(char* filename, int** maze) {
     FILE *file;
     file = fopen(filename, "r");
@@ -11,7 +13,7 @@ void readFile(char* filename, int** maze) {
         fscanf(file, "%c", &c);
         if (c != '\n') {
             maze[x][y++] = c - '0';
-            if (y == 10) {
+            if (y == LENGTH) {
                 y = 0;
                 x++;
             }
@@ -21,20 +23,49 @@ void readFile(char* filename, int** maze) {
 }
 
 void printMaze(int** maze) {
-    for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 10; y++) {
+    for (int x = 0; x < LENGTH; x++) {
+        for (int y = 0; y < LENGTH; y++) {
             printf("%d", maze[x][y]);
         }
         printf("\n");
     }
 }
 
+void freeMaze(int** maze) {
+    for (int i = 0; i < LENGTH; i++) {
+        free(maze[i]);
+    }
+}
+
+int getBestPath(int** maze) {
+    int* path = malloc(sizeof(int) * LENGTH * LENGTH);
+    int* current[LENGTH];
+    for (int i = 0; i < LENGTH; i++) {
+        current[i] = (int*)malloc(LENGTH * sizeof(int));
+    }
+    for (int x = 0; x < LENGTH; y++) {
+        for (int y = 0; y < LENGTH; y++) {
+            current[i] = 999;
+        }
+    }
+    int bestRisk = getBestPathRecursive(maze, current, 0, 0, path, 0, -1 * maze[0][0]);
+    freeMaze(current);
+    return bestRisk;
+}
+
+int getBestPathRecursive(int** maze, int** current, int x, int y, int* path, int pathLength, int risk) {
+    int newRisk = risk + maze[x][y];
+    return newRisk;
+    int lowestRisk = getBestPathRecursive();
+}
+
 int main(int argc, char* argv[]) {
-    int* maze[10];
-    for (int i = 0; i < 10; i++) {
-        maze[i] = (int*)malloc(10 * sizeof(int));
+    int* maze[LENGTH];
+    for (int i = 0; i < LENGTH; i++) {
+        maze[i] = (int*)malloc(LENGTH * sizeof(int));
     }
     readFile(argv[1], maze);
     printMaze(maze);
+    freeMaze(maze);
     return 0;
 }
